cd2442af1219a04a483f7d4281c87f022950693b
Uzay Macar <uzay.macar@gmail.com>
2024-05-25 18:24:47 +0100

    Implement index for components for modular usage.

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as Button } from './button/Button';
+export { default as Form } from './form/Form';
+export { default as Input } from './input/Input';
+export { default as Redirector } from './redirector/Redirector';
+export { default as Sidebar } from './sidebar/Sidebar';
