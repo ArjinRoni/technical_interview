d0ee5018c228aeb16521c8955f655ba93a9443bb
Uzay Macar <uzay.macar@gmail.com>
2024-05-25 18:25:22 +0100

    Implement login and signup pages.

---
+++ src/app/login/page.js
@@ -0,0 +1,54 @@
+'use client';
+import React, { useState } from 'react';
+
+import { Form, Input, Redirector, Button } from '@/components';
+import { checkError } from '@/utils/ErrorHandler';
+import { useAuth } from '@/contexts/AuthContext';
+
+const Login = () => {
+  const { login } = useAuth();
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const hasError = checkError([
+      {
+        condition:
+          !email ||
+          email.trim().length === 0 ||
+          !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w\w+)+$/.test(email.trim()),
+        message: 'Please enter a valid email.',
+      },
+      {
+        condition: !password || password.length < 6,
+        message: 'Please enter a password with a minimum length of 6 characters',
+      },
+    ]);
+    if (hasError) return;
+
+    await login({ email, password });
+  };
+
+  return (
+    <div className="page">
+      <Form title="Welcome back 👋" onSubmit={handleSubmit}>
+        <Input
+          type="email"
+          placeholder="Email"
+          autocomplete="email"
+          pattern="[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,}$"
+          value={email}
+          setValue={setEmail}
+        />
+        <Input type="password" placeholder="Password" value={password} setValue={setPassword} />
+        <Redirector text="New around here? Sign up instead →" href="/signup" />
+        <Button type="submit" text="Login" />
+      </Form>
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/app/signup/page.js
@@ -0,0 +1,69 @@
+'use client';
+import React, { useState } from 'react';
+
+import '../../styles/page.css';
+
+import { Form, Input, Redirector, Button } from '@/components';
+import { checkError } from '@/utils/ErrorHandler';
+import { useAuth } from '../../contexts/AuthContext';
+
+const SignUp = () => {
+  const { signup } = useAuth();
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const hasError = checkError([
+      {
+        condition:
+          !email ||
+          email.trim().length === 0 ||
+          !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w\w+)+$/.test(email.trim()),
+        message: 'Please enter a valid email.',
+      },
+      {
+        condition: !password || password.length < 6,
+        message: 'Please enter a password with a minimum length of 6 characters',
+      },
+      {
+        condition: !confirmPassword || password !== confirmPassword,
+        message: 'Confirm password does not match.',
+      },
+    ]);
+    if (hasError) return;
+
+    await signup({ email: email.trim(), password });
+  };
+
+  return (
+    <div className="page">
+      <Form title="Join the future of advertising now 🚀" onSubmit={handleSubmit}>
+        <Input type="text" placeholder="Name" autocomplete="name" value={name} setValue={setName} />
+        <Input
+          type="email"
+          placeholder="Email"
+          autocomplete="email"
+          pattern="[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,}$"
+          value={email}
+          setValue={setEmail}
+        />
+        <Input type="password" placeholder="Password" value={password} setValue={setPassword} />
+        <Input
+          type="password"
+          placeholder="Confirm password"
+          value={confirmPassword}
+          setValue={setConfirmPassword}
+        />
+        <Redirector text="Already have an account? Log in instead →" href="/login" />
+        <Button type="submit" text="Sign up" />
+      </Form>
+    </div>
+  );
+};
+
+export default SignUp;
