a74e48511561148519882ada980f566bf48ec843
Uzay Macar <uzay.macar@gmail.com>
2024-05-25 18:23:34 +0100

    Context for sharing fonts.

---
+++ src/contexts/FontContext.js
@@ -0,0 +1,19 @@
+'use client';
+import { createContext } from 'react';
+import { Inter, Hanken_Grotesk } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
+const hg = Hanken_Grotesk({ subsets: ['latin'] });
+
+export const FontContext = createContext({
+  primaryFont: null,
+  secondaryFont: null,
+});
+
+export const FontProvider = ({ children }) => {
+  return (
+    <FontContext.Provider value={{ primaryFont: hg, secondaryFont: inter }}>
+      <main className={inter.className}>{children}</main>
+    </FontContext.Provider>
+  );
+};
