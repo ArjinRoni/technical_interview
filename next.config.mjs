281325b74ae77f014bc53f6fc31c79d4b96c2755
Uzay Macar <uzay.macar@gmail.com>
2024-05-26 14:58:37 +0100

    Install OpenAI library + implement environment variables.

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+    ASSISTANT_ID: process.env.ASSISTANT_ID,
+  },
+};
 
 export default nextConfig;
