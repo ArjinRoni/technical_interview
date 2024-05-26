442854b63c13a4e566ac6c8350d2374d2397e59d
Uzay Macar <uzay.macar@gmail.com>
2024-05-27 00:18:36 +0100

    Implement chat component for the sidebar.

---
+++ src/components/chat/Chat.js
@@ -0,0 +1,22 @@
+'use client';
+import './chat.css';
+
+import { usePathname } from 'next/navigation';
+import Link from 'next/link';
+
+const Chat = ({ chat }) => {
+  // Check if the chat is currently active and on screen for the user
+  const pathname = usePathname();
+  const isActive = pathname === `/chat/${chat.chatNo}`;
+
+  return (
+    <Link href={`/chat/${chat.chatNo}`}>
+      <div className={`chat ${isActive ? 'chat-selected' : ''}`}>
+        <img style={{ width: 24, height: 24 }} src="/double-chat-bubble.png" />
+        <p className="chat-title">{chat.title}</p>
+      </div>
+    </Link>
+  );
+};
+
+export default Chat;
