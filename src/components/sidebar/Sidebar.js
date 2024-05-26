1d1e810a2061b372eb01b545c06cb8b3a98cccee
Uzay Macar <uzay.macar@gmail.com>
2024-05-26 15:00:29 +0100

    WIP sidebar push.

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,12 +1,26 @@
 'use client';
 import './sidebar.css';
+import { useRouter } from 'next/navigation';
 
 import Button from '../button/Button';
 
 import { useAuth } from '@/contexts/AuthContext';
+import { useChats } from '@/contexts/ChatsContext';
+import { useMadison } from '@/contexts/MadisonContext';
 
 const Sidebar = () => {
+  const router = useRouter();
   const { logout } = useAuth();
+  const { chats, createChat } = useChats();
+  const { createThread, createRun } = useMadison();
+
+  const createNewChatAndNavigate = async () => {
+    const threadId = await createThread();
+    await createRun(threadId);
+    const chatId = await createChat(threadId);
+
+    router.push(`/chat/${chatId}`);
+  };
 
   const SidebarButton = ({ src, text, onClick }) => {
     return (
@@ -27,8 +41,9 @@ const Sidebar = () => {
         type="button"
         text="New ad"
         emoji="rocket"
+        onClick={createNewChatAndNavigate}
       />
-      <div className="sidebar-chats"></div>
+      <div className="sidebar-chats">{chats.map((x) => x.id)}</div>
       <div className="sidebar-buttons">
         <SidebarButton src="./settings.png" text="Settings" />
         <SidebarButton src="./user.png" text="My account" />
