44407eeb14d29170a4da9eaab0ebf86fb30dcd9a
Uzay Macar <uzay.macar@gmail.com>
2024-05-26 15:00:16 +0100

    Hook for checking for messages.

--- src/app/chat/[id]/page.js
+++ src/app/chat/[id]/page.js
@@ -1,7 +1,32 @@
-import React from 'react';
+'use client';
+import React, { useEffect } from 'react';
+
+import { useMadison } from '@/contexts/MadisonContext';
 
 export default function Page({ params }) {
+  const { currentRun } = useMadison();
+
   const { id } = params;
+
+  useEffect(() => {
+    const checkForMessages = async (run) => {
+      if (run.status === 'completed') {
+        const messages = await openai.beta.threads.messages.list(run.thread_id);
+        for (const message of messages.data.reverse()) {
+          console.log(`${message.role} > ${message.content[0].text.value}`);
+        }
+      } else {
+        console.log(run.status);
+      }
+    };
+
+    currentRun && checkForMessages(currentRun);
+  }, [currentRun]);
+
+  /*
+          await setDoc(doc(db, 'chats', userId, chatId, messageId), {
+          text: 'Hello, how can I help you?',
+        }); */
 
   return (
     <div>
