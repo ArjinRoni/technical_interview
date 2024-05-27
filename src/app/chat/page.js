77756fcff112ebda5a9566b1a6dc68c12a39e191
Uzay Macar <uzay.macar@gmail.com>
2024-05-27 03:49:58 +0100

    Redirect from unused page / path.

--- src/app/chat/page.js
+++ src/app/chat/page.js
@@ -1,17 +1,27 @@
 'use client';
-import React from 'react';
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 
-import { Sidebar, UserInput } from '@/components';
+import { useAuth } from '@/contexts/AuthContext';
 
-import { useChats } from '@/contexts/ChatsContext';
+// NOTE: We don't serve anything at /chat at this moment, hence we redirect the user to /dashboard
+const ChatPage = () => {
+  const { user, isAuthLoading } = useAuth();
+  const router = useRouter();
 
-const Chat = () => {
-  return (
-    <div>
-      <Sidebar />
-      <UserInput />
-    </div>
-  );
+  useEffect(() => {
+    if (isAuthLoading) return; // Don't take an action when when the auth is still loading
+
+    if (user) {
+      // User is signed in, redirect to the dashboard
+      router.push('/dashboard');
+    } else {
+      // User is not signed in, redirect to the login page
+      router.push('/login');
+    }
+  }, [user, isAuthLoading, router]);
+
+  return null;
 };
 
-export default Chat;
+export default ChatPage;
