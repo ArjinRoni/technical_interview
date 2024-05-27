a407f3a57da7c95176603a97478e338d997551d2
Uzay Macar <uzay.macar@gmail.com>
2024-05-27 10:37:01 +0100

    Implement password mask removal for convenience.

--- src/components/input/Input.js
+++ src/components/input/Input.js
@@ -1,17 +1,38 @@
-import React from 'react';
+'use client';
+import React, { useState } from 'react';
 import './input.css';
 
 const Input = ({ type, placeholder, pattern, autocomplete, value, setValue }) => {
+  const [isVisible, setIsVisible] = useState(type !== 'password');
+  const [inputType, setInputType] = useState(type);
+
   return (
-    <input
-      className="input"
-      type={type}
-      placeholder={placeholder}
-      pattern={pattern}
-      autoComplete={autocomplete}
-      value={value}
-      onChange={(e) => setValue(e.target.value)}
-    />
+    <div className="input-container">
+      <input
+        className="input"
+        type={inputType}
+        placeholder={placeholder}
+        pattern={pattern}
+        autoComplete={autocomplete}
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+      {type === 'password' && (
+        <img
+          className="input-visibility-toggler"
+          src={isVisible ? '/eye-off.png' : '/eye.png'}
+          onClick={() => {
+            const newValue = !isVisible;
+            setIsVisible(newValue);
+            if (newValue) {
+              setInputType('text');
+            } else {
+              setInputType('password');
+            }
+          }}
+        />
+      )}
+    </div>
   );
 };
 
