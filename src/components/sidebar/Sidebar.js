b3b088c39271f20718996c3288441858da056134
Uzay Macar <uzay.macar@gmail.com>
2024-05-27 03:49:00 +0100

    Implement sidebar close and open functionality.

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,5 +1,5 @@
 'use client';
-import './sidebar.css';
+import React from 'react';
 import { useRouter } from 'next/navigation';
 
 import Chat from '../chat/Chat';
@@ -8,19 +8,28 @@ import Button from '../button/Button';
 import { useAuth } from '@/contexts/AuthContext';
 import { useChats } from '@/contexts/ChatsContext';
 import { useMadison } from '@/contexts/MadisonContext';
+import { useUI } from '@/contexts/UIContext';
+
+import './sidebar.css';
 
 const Sidebar = () => {
   const router = useRouter();
   const { logout } = useAuth();
   const { chats, createChat } = useChats();
   const { createThread, createRun } = useMadison();
+  const { isSidebarOpen, setIsSidebarOpen, setIsLoading, setLoadingMessage } = useUI();
 
+  // Function to create a new chat and navigate to the correct chat path
   const createNewChatAndNavigate = async () => {
+    setIsLoading(true);
+    setLoadingMessage(`Hang on tight... We're creating a new chat for you 🚀 `);
+
     const threadId = await createThread();
     await createRun(threadId);
     const chatNo = await createChat(threadId);
-
     router.push(`/chat/${chatNo}`);
+
+    setIsLoading(false);
   };
 
   const SidebarButton = ({ src, text, onClick }) => {
@@ -33,27 +42,44 @@ const Sidebar = () => {
   };
 
   return (
-    <div className="sidebar">
-      <Button
-        borderRadius={8}
-        fontSize={16}
-        width="80%"
-        alignSelf="center"
-        type="button"
-        text="New ad"
-        emoji="rocket"
-        onClick={createNewChatAndNavigate}
-      />
-      <div className="sidebar-chats">
-        {chats.map((chat) => (
-          <Chat key={chat.chatNo} chat={chat} />
-        ))}
-      </div>
-      <div className="sidebar-buttons">
-        <SidebarButton src="/settings.png" text="Settings" />
-        <SidebarButton src="/user.png" text="My account" />
-        <SidebarButton src="/email-plus.png" text="Send feedback" />
-        <SidebarButton src="/log-out.png" text="Log out" onClick={async () => await logout()} />
+    <div className="sidebar" style={{ width: isSidebarOpen ? 216 : 24 }}>
+      {isSidebarOpen && (
+        <>
+          <Button
+            borderRadius={8}
+            fontSize={16}
+            width="80%"
+            alignSelf="center"
+            type="button"
+            text="New ad"
+            emoji="rocket"
+            onClick={createNewChatAndNavigate}
+          />
+          <div className="sidebar-chats">
+            {chats.map((chat) => (
+              <Chat key={chat.chatNo} chat={chat} />
+            ))}
+          </div>
+          <div className="sidebar-buttons">
+            <SidebarButton src="/settings.png" text="Settings" />
+            <SidebarButton src="/user.png" text="My account" />
+            <SidebarButton src="/email-plus.png" text="Send feedback" />
+            <SidebarButton src="/log-out.png" text="Log out" onClick={async () => await logout()} />
+          </div>
+        </>
+      )}
+      <div
+        className="sidebar-open-button"
+        style={{
+          borderTopLeftRadius: isSidebarOpen ? 12 : 0,
+          borderBottomLeftRadius: isSidebarOpen ? 12 : 0,
+        }}
+        onClick={() => setIsSidebarOpen(!isSidebarOpen)}
+      >
+        <img
+          style={{ width: 18, height: 18 }}
+          src={isSidebarOpen ? '/arrow-left-white.png' : '/arrow-right-white.png'}
+        />
       </div>
     </div>
   );
