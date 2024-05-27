674de8544fe0cfaf1ba06d73c8d2f697c316b1b4
Uzay Macar <uzay.macar@gmail.com>
2024-05-27 07:28:09 +0100

    Update spinner component to be adaptable to different backgrounds.

--- src/components/spinner/Spinner.js
+++ src/components/spinner/Spinner.js
@@ -1,7 +1,16 @@
 import './spinner.css';
 
-const Spinner = ({ size = 32 }) => {
-  return <div style={{ width: size, height: size }} className="spinner" />;
+const Spinner = ({ size = 32, marginTop = 20, isGray = false }) => {
+  return (
+    <div
+      style={{
+        width: size,
+        height: size,
+        marginTop,
+      }}
+      className={isGray ? 'spinner-gray' : 'spinner-white'}
+    />
+  );
 };
 
 export default Spinner;
